609da15b9953611ab24cd9cd701e07c3dc66f107
bigweerab1984-creator <bigweerab1984@gmail.com>
2026-06-12 19:09:53 +0100

    Create src/components/index.js

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { Spinner } from "./Spinner";
+export { Tag } from "./Tag";
+export { KPI } from "./KPI";
+export { MiniBar } from "./MiniBar";
+export { Sidebar } from "./Sidebar";
+export { TopBar } from "./TopBar";
